a08fd4b61c66e2ca3f14b2b24cf30e17545eaaf6
rohanprajapati <rohanprajapati369@gmail.com>
2023-12-11 12:09:21 +0545

    feat: show menu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 /* Menu Show Y Hidden */
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+/* Menu Show */
+/* validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add("show-menu")
+    })
+}
