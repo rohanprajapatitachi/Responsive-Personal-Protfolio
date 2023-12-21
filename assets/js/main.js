77e947b18884d6a84a34ebab3b7bc943fa1f3c10
rohanprajapati <rohanprajapati369@gmail.com>
2023-12-22 01:38:47 +0545

    added js to toggle open and close

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,22 @@ function linkAction(){
     navMenu.classList.remove("show-menu");
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* Accordion Skills */
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+}
