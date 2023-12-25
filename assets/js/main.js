383bb76539b89b9554b7644d8f5e56225e12b6fd
rohanprajapati <rohanprajapati369@gmail.com>
2023-12-25 13:39:01 +0545

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,17 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
+// Qualificatio Tabs
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target);
+        
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        })
+    })
+})
