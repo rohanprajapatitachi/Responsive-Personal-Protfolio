7738b4cd1b343a47f1d452e233629f918af6f46f
rohanprajapati <rohanprajapati369@gmail.com>
2023-12-29 00:56:28 +0545

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,17 @@ const modalCloses = document.querySelectorAll('.services__modal-close');
 let modal = function(modalClick){
     modalViews[modalClick].classList.add('active-modal');
 }
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i);
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
