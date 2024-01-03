ca168ad4e2883be885d53a00892e5c8b04d2547a
rohanprajapati <rohanprajapati369@gmail.com>
2024-01-04 01:08:38 +0545

    Update Swiper container and pagination in index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,7 @@ modalCloses.forEach((modalClose) =>{
 })
 
 // PORTFOLIO SWIPER
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
     navigation: {
@@ -104,6 +104,24 @@ let swiper = new Swiper(".portfolio__container", {
     },
 });
 
+// Testimonial
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor:true,
+    spaceBetween:48,
+
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+      dynamicBullets:true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView:2,
+        }
+    }
+});
 
 
 
