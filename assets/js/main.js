a071b1333d67c627192638261a65c0351c8944b6
rohanprajapati <rohanprajapati369@gmail.com>
2024-01-06 16:28:34 +0545

    Add scrollHeader event listener and footer spacing

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,5 +149,6 @@ function scrollHeader(){
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag //
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
 }
+window.addEventListener('scroll', scrollHeader);
 
 
